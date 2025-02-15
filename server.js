f2cff3cdbeba099ca5cc14b9f6e9145f39cc033b
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2025-02-15 11:38:08 -0800

    .

---
+++ server.js
@@ -0,0 +1,81 @@
+let http = require('http');
+let db = require('mysql2');
+let url = require('url');
+
+
+let con = db.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "q12773250P"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+con.query("DROP DATABASE IF EXISTS Clinic", function (err, result) {
+    if (err) throw err;
+    console.log("Database dropped");
+});
+con.query("CREATE DATABASE Clinic", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+    con.query("USE Clinic", function (err, result) {
+        con.query("CREATE TABLE Patients (patientID INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), dateOfBirth DATETIME)", function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    });
+});
+http.createServer(function (req, res) {
+
+    let q = url.parse(req.url, true);
+    if (req.method === "POST" && q.pathname === "/insertPredefined") {
+        res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            let patientData = JSON.parse(body);
+            console.log(patientData);
+            let sql = "INSERT INTO Patients (name, dateOfBirth) VALUES ?";
+            let values = patientData.map(patient => [patient.name, patient.dateofbirth]);
+            con.query(sql, [values], function (err, result) {
+                if (err) throw err;
+                res.write("Data was inserted successfully!");
+                res.end();
+            });
+        });
+    } else if (q.pathname === "/lab5/api/v1/sql") {
+
+        let q = url.parse(req.url, true);
+        let sqlQuery = q.query.query;
+        if (req.method === "GET" || req.method === "POST") {
+            con.query(sqlQuery, function (err, result, fields) {
+                if (err) {
+                    res.writeHead(400, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(result));
+                    return;
+                }
+                res.writeHead(200, { 'Content-Type': 'application/json' , 'Access-Control-Allow-Origin': '*' });
+                if (req.method === "GET") {
+                    res.end(JSON.stringify(result));
+                } else {
+                    res.end("Query executed successfully");
+                }
+            });
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
+            res.end("Only GET and POST methods are allowed");
+        }
+
+
+    }
+
+    if (req.method === "drop" || req.method === "UPDATE") {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.write("Drop and update is not allowed");
+    }
+
+}).listen(8080);
